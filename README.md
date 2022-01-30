ff4347a56ad6a21beb0c3ef0662cda7e653361ce
cutaway <cutawaysecurity@gmail.com>
2022-01-30 13:28:19 -0800

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# dnp3stalker
-Tools for interacting with DNP3 devices
+# DNP3 Stalker
+
+## Purpose
+
+DNP3 Stalker is a project to analyze and interact with DNP3 devices.
+
+## Modules and Scripts
+
+* Goose
+  * dnp3stalker_serial.py - Interact with DNP3 devices via serial connections.
+
+## Requirements
+
+* CRCMOD - module for computing DNP Cyclic Redundancy Check (CRC) which is CRC-16 implementation specific to DNP. 
+* Serial - module for serial communications
---
+++ dnp3stalker_serial.py
@@ -0,0 +1,178 @@
+import sys
+import serial
+import time
+import crcmod.predefined
+
+####################
+# DNP Command Setup
+# NOTE: Use strings in hex where you can to be consistent with 
+#       bytes.fromhex(str) for functions.
+####################
+src_address = 1023
+dst_address = 1
+SRC_ADDR    = src_address.to_bytes(2,'little')
+DST_ADDR    = dst_address.to_bytes(2,'little')
+DNP_HEADER  = '0564'
+
+####################
+# Helper Functions DNP Commands
+####################
+# Generate DNP3 CRC
+def gen_crc(data):
+    # Import DNP3 CRC
+    # NOTE: Normal use of CRCMOD and CRCCHECK did not generate the CRC correctly
+    #       This is the ONLY method that generated the CRC correctly
+    #       Online CRC Check: https://www.lammertbies.nl/comm/info/crc-calculation
+    crcdnp = crcmod.predefined.mkCrcFun('crc-16-dnp')
+    return crcdnp(data).to_bytes(2,'little')
+
+# Build Header Packet
+def build_dnp_header(head,src,dst,cntl):
+    # Length will have to be updated if additional data is placed in packet
+    plen    = 5
+    packet = bytes.fromhex(head) + plen.to_bytes(1,'little') + bytes.fromhex(cntl) + dst.to_bytes(2,'little') + src.to_bytes(2,'little') 
+    crc    = gen_crc(packet)
+    return packet + crc
+
+# Build Object Packet
+def build_dnp_object(data):
+    # Length will have to be updated if additional data is placed in packet
+    plen    = 5
+    packet = bytes.fromhex(data)
+    crc    = gen_crc(packet)
+    return packet + crc
+
+####################
+# Full DNP Commands
+####################
+# Data Link Layer Control Codes
+## Producer
+DLLCC_P_ACK                   = '80'
+DLLCC_P_NACK                  = '81'
+DLLCC_P_LINK_STATUS           = '8B'
+DLLCC_P_NOT_SUPPORTED         = '8F'
+DLLCC_P_RESET_LINK_STATES     = 'C0'
+DLLCC_P_UNCONFIRMED_USER_DATA = 'C4'
+DLLCC_P_REQUEST_LINK_STATUS   = 'C9'
+DLLCC_P_TEST_LINK_STATES      = 'D2'
+DLLCC_P_CONFIRMED_USER_DATA_D = 'D3'
+DLLCC_P_CONFIRMED_USER_DATA_F = 'F3'
+## Consumer
+DLLCC_O_ACK                   = '00'
+DLLCC_O_NACK                  = '01'
+DLLCC_O_LINK_STATUS           = 'OF'
+DLLCC_O_NOT_SUPPORTED         = '0F'
+DLLCC_O_RESET_LINK_STATES     = '40'
+DLLCC_O_UNCONFIRMED_USER_DATA = '44'
+DLLCC_O_REQUEST_LINK_STATUS   = '49'
+DLLCC_O_TEST_LINK_STATES      = '52'
+DLLCC_O_CONFIRMED_USER_DATA_D = '53'
+DLLCC_O_CONFIRMED_USER_DATA_F = '73'
+
+# Function Codes
+FC_CONFIRM                = '00'
+FC_READ                   = '01'
+FC_WRITE                  = '02'
+FC_SELECT                 = '03'
+FC_OPERATOR               = '04'
+FC_DIR_OPERATE            = '05'
+FC_DIR_OPERATE_NO_RESP    = '06'
+FC_FREEZE                 = '07'
+FC_FREEZE_NO_RESP         = '08'
+FC_FREEZE_CLEAR           = '09'
+FC_FREEZE_CLEAR_NO_RESP   = '0A'
+FC_FREEZE_AT_TIME         = '0B'
+FC_FREEZE_AT_TIME_NO_RESP = '0C'
+FC_COLD_RESTART           = '0D'
+FC_WARM_RESTART           = '0E'
+FC_INIT_DATA              = '0F'
+FC_INIT_APP               = '10'
+FC_START_APP              = '11'
+FC_STOP_APP               = '12'
+FC_SAVE_CONFIG            = '13'
+FC_ENABLE_UNSOL           = '14'
+FC_DISABLE_UNSOL          = '15'
+FC_ASSIGN_CLASS           = '16'
+FC_DELAY_MEASURE          = '17'
+FC_RECORD_TIME            = '18'
+FC_OPEN_FILE              = '19'
+FC_CLOSE_FILE             = '1A'
+FC_DELETE_FILE            = '1B'
+FC_FILE_INFO              = '1C'
+FC_AUTH_FILE              = '1D'
+FC_ABORT_FILE             = '1E'
+FC_ACTIVATE_CONFIG        = '1F'
+FC_AUTH_REQ               = '20'
+FC_AUTH_REQ_NO_ACK        = '21'
+FC_RESP                   = '81'
+FC_UNSOL_RESP             = '82'
+FC_AUTH_RESP              = '83'
+
+
+# Broadcast Commands
+COLD_RESTART_BROADCAST = '056408C4FFFFFFFF4451C0C00D9C86'
+LINK_STATUS_BROADCAST  = '056405C9FFFFFFFF46C9'
+
+# Build commands
+LINK_STATUS_DIRECT      = build_dnp_header(DNP_HEADER,src_address,dst_address,DLLCC_P_REQUEST_LINK_STATUS)
+RESET_LINK_STATE_DIRECT = build_dnp_header(DNP_HEADER,src_address,dst_address,DLLCC_P_RESET_LINK_STATES)
+TEST_LINK_STATE_DIRECT  = build_dnp_header(DNP_HEADER,src_address,dst_address,DLLCC_P_TEST_LINK_STATES)
+
+# Wrapper for sending broadcast messages
+# s = open serial port
+# cmd = string of hex bytes
+def send_broadcast(s, cmd):
+    s.write(bytes.fromhex(cmd))
+    time.sleep(1)
+    r = s.read(size=200)
+    if r: print(r)
+    time.sleep(1)
+
+# Wrapper for sending direct messages
+# s = open serial port
+# cmd = byte string built from build_dnp_header function
+# cmd = byte string built from build_dnp_object
+def send_direct(s, cmd, obj=''):
+    # If there are DNP3 objects, update the length byte
+    # NOTE: DNP3 objects should be completely formed with CRC
+    len_index = 2
+    if obj:
+        cmd = cmd[:index] + (cmd[index] + len(obj)).to_bytes(1,'little') + cmd[index + 1:]
+    s.write(cmd + obj)
+    time.sleep(1)
+    r = s.read(size=200)
+    if r: print(r)
+    time.sleep(1)
+
+###############
+# Setup Serial
+###############
+port     = '/dev/ttyUSB0'
+baudrate = 19200
+timeout  = 1
+bytesize = 8
+stopbits = serial.STOPBITS_ONE
+serialPort = serial.Serial(port=port, baudrate=baudrate,
+                                bytesize=bytesize, timeout=timeout, stopbits=stopbits)
+response = b''
+
+while True:
+    try:
+        serialPort.write(bytes.fromhex(COLD_RESTART_BROADCAST))
+        time.sleep(1)
+        response = serialPort.read(size=200)
+        if response: print(response)
+        time.sleep(1)
+        serialPort.write(build_dnp_data_header(DNP_HEADER,src_address,dst_address,'C9'))
+        print("%s"%(build_dnp_data_header(DNP_HEADER,src_address,dst_address,'C9').hex()))
+        time.sleep(1)
+        # serialPort.reset_input_buffer()
+        # serialPort.reset_output_buffer()
+        # serialString = serialPort.read(10).hex()
+        response = serialPort.read(size=200)
+        if response: print(response)
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
+
+serialPort.close()
